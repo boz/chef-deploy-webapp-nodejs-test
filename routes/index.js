34dfc096933398c6b1d0fd705a2d196127fed88b
Adam Bozanich <adam.boz@gmail.com>
2013-02-04 10:05:28 -0800

    connect to redis

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,30 @@
+var config  = require('config').load(),
+       util = require('util');
+
+var redis, redisClient;
+
+if(config.redis) {
+  redis = require('redis').createClient(config.redis.port,config.redis.host);
+}
 
 /*
  * GET home page.
  */
 
+function renderIndex(req,res,config,redisInfo) {
+  res.render('index', {
+    title: 'chef-deploy test nodejs',
+    config: util.inspect(config),
+    redisInfo: redisInfo 
+  });
+}
+
 exports.index = function(req, res){
-  res.render('index', { title: 'chef-deploy test nodejs' });
+  if(redis) {
+    redis.info(function(err,info){
+      renderIndex(req,res,config,info || err);
+    });
+  } else {
+    renderIndex(req,res,config);
+  }
 };
